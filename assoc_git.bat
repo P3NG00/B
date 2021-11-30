0d2143ea6fe81bf7ef16217cfaa7e1b734f5e61c
P3 <p3ng00minecraft@gmail.com>
2021-11-30 09:47:40 -0700

    references to this USB will use Y as drive

--- assoc_git.bat
+++ assoc_git.bat
@@ -1,4 +1,3 @@
 @echo off
-set /p driveLetter=Enter drive letter: 
-set git=%driveLetter%:\installations\PortableGit\bin\git.exe
+set git=Y:\installations\PortableGit\bin\git.exe
 echo use %%git%% to reference '%git%'
