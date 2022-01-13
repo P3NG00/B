bcdcdd914a05129302f3db098eea5b82d644485b
P3 <p3ng00minecraft@gmail.com>
2022-01-13 16:28:28 -0700

    removed unused Vector2 variable

--- src/Utils/Vector2.cs
+++ src/Utils/Vector2.cs
@@ -7,7 +7,6 @@ namespace B.Utils
         public static readonly Vector2 Left = new Vector2(-1, 0);
         public static readonly Vector2 Right = new Vector2(1, 0);
         public static readonly Vector2 Down = new Vector2(0, -1);
-        public static readonly Vector2 Zero = new Vector2(0);
 
         public int x;
         public int y;
