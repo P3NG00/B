68d918f14a271812e66e1fd8c2c0020c245365d7
P3 <p3ng00minecraft@gmail.com>
2022-02-22 09:11:22 -0700

    fixed serializing togglables

--- src/Utils/Togglable.cs
+++ src/Utils/Togglable.cs
@@ -3,14 +3,12 @@ namespace B.Utils
     [Serializable]
     public sealed class Togglable
     {
-        private bool _value;
-
-        public bool Active => this._value;
+        public bool Active = false;
 
         public Togglable() : this(false) { }
 
-        public Togglable(bool value) => this._value = value;
+        public Togglable(bool value) => this.Active = value;
 
-        public void Toggle() => this._value = !this._value;
+        public void Toggle() => this.Active = !this.Active;
     }
 }
