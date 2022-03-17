1aacb253743194cc24dad31581a9b3aefacf724e
P3 <p3ng00minecraft@gmail.com>
2022-03-17 15:59:25 -0600

    implemented external code to disable text selection mode

--- src/Utils/External.cs
+++ src/Utils/External.cs
@@ -12,16 +12,35 @@ namespace B.Utils
         {
             // Copied Code to disable some window resizing functionality
             // https://social.msdn.microsoft.com/Forums/vstudio/en-US/1aa43c6c-71b9-42d4-aa00-60058a85f0eb/c-console-window-disable-resize?forum=csharpgeneral
-            IntPtr handle = External.GetConsoleWindow();
+            IntPtr handle = GetConsoleWindow();
 
             if (handle != IntPtr.Zero)
             {
-                IntPtr sysMenu = External.GetSystemMenu(handle, false);
-                Enum.GetValues<SystemCommand>().ForEach(sc => External.DeleteMenu(sysMenu, (int)sc, 0x00000000));
+                IntPtr sysMenu = GetSystemMenu(handle, false);
+                Enum.GetValues<SystemCommand>().ForEach(sc => DeleteMenu(sysMenu, (int)sc, 0x00000000));
             }
+            else
+                throw new Exception("Failed to get console window handle.");
+
+            // Copied Code to disable text selection functionality
+            // https://stackoverflow.com/a/36720802
+
+            // -10 is the standard input device
+            handle = GetStdHandle(-10);
+
+            if (GetConsoleMode(handle, out uint consoleMode))
+            {
+                // 0x0040 controls text selection (quick edit mode)
+                consoleMode &= ~(uint)(0x0040);
+
+                if (!SetConsoleMode(handle, consoleMode))
+                    throw new Exception("Failed to set console mode.");
+            }
+            else
+                throw new Exception("Failed to get console mode.");
         }
 
-        public enum SystemCommand
+        private enum SystemCommand
         {
             SC_SIZE = 0xF000,
             SC_MINIMIZE = 0xF020,
@@ -30,12 +49,21 @@ namespace B.Utils
         }
 
         [DllImport(USER32)]
-        public static extern int DeleteMenu(IntPtr hMenu, int nPosition, int wFlags);
+        private static extern int DeleteMenu(IntPtr hMenu, int nPosition, int wFlags);
 
         [DllImport(USER32)]
-        public static extern IntPtr GetSystemMenu(IntPtr hWnd, bool bRevert);
+        private static extern IntPtr GetSystemMenu(IntPtr hWnd, bool bRevert);
 
         [DllImport(KERNEL32, ExactSpelling = true)]
-        public static extern IntPtr GetConsoleWindow();
+        private static extern IntPtr GetConsoleWindow();
+
+        [DllImport(KERNEL32, SetLastError = true)]
+        private static extern IntPtr GetStdHandle(int nStdHandle);
+
+        [DllImport(KERNEL32)]
+        private static extern bool GetConsoleMode(IntPtr hConsoleHandle, out uint lpMode);
+
+        [DllImport(KERNEL32)]
+        private static extern bool SetConsoleMode(IntPtr hConsoleHandle, uint dwMode);
     }
 }
