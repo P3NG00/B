da8a74276aa9f7ff08aa440f1d732711255c2fd2
p3ng00 <p3ng00minecraft@gmail.com>
2022-04-18 22:44:21 -0600

    added comments and spacing to mouse class

--- src/Utils/Mouse.cs
+++ src/Utils/Mouse.cs
@@ -2,18 +2,24 @@ namespace B.Utils
 {
     public static class Mouse
     {
-        private static Vector2 CharSize = new(8, 16);
-        private static Vector2 WindowTrueStart => new Vector2(1, 2) * CharSize;
+        #region Universal Properties
 
+        // This represents the size of one char in the console
+        public static Vector2 CharSize => new(8, 16);
         // This will return the position of the mouse relative
         // to the top-left corner of the text window
-        public static Vector2 Position
-        {
-            get
-            {
-                Vector2 mousePos = External.GetRelativeMousePosition();
-                return mousePos - WindowTrueStart;
-            }
-        }
+        public static Vector2 Position => External.GetRelativeMousePosition() - TextBeginOffset;
+
+        #endregion
+
+
+
+        #region Private Properties
+
+        // This represents the offset from the true top left corner of
+        // the window to the top-left corner of where the text begins.
+        private static Vector2 TextBeginOffset => new Vector2(1, 2) * CharSize;
+
+        #endregion
     }
 }
