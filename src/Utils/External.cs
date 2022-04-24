2a5b4df21f0db6a16f0e17e7193b2873bef6be12
p3ng00 <p3ng00minecraft@gmail.com>
2022-04-23 23:58:50 -0600

    organized external code. added unused title bar disabler

--- src/Utils/External.cs
+++ src/Utils/External.cs
@@ -16,6 +16,7 @@ namespace B.Utils
 
         #region Cache
 
+        // This is cached because it is used in GetRelativeMousePosition()
         private static IntPtr _consoleHandle;
 
         #endregion
@@ -26,32 +27,9 @@ namespace B.Utils
 
         public static void Initialize()
         {
-            // Disable Window Resizing Functionality
-            // https://social.msdn.microsoft.com/Forums/vstudio/en-US/1aa43c6c-71b9-42d4-aa00-60058a85f0eb/c-console-window-disable-resize?forum=csharpgeneral
-            _consoleHandle = GetConsoleWindow();
-
-            if (_consoleHandle != IntPtr.Zero)
-            {
-                IntPtr sysMenu = GetSystemMenu(_consoleHandle, false);
-                Enum.GetValues<SystemCommand>().ForEach(sc => DeleteMenu(sysMenu, (int)sc, 0x0000_0000));
-            }
-            else
-                throw new Exception("Failed to get console window handle.");
-
-            // Disable Text Selection Functionality
-            // https://stackoverflow.com/a/36720802
-
-            // -10 is the standard input device
-            IntPtr currentHandle = GetStdHandle(-10);
-
-            if (!GetConsoleMode(currentHandle, out uint consoleMode))
-                throw new Exception("Failed to get console mode.");
-
-            // 0b1000000 (quick edit mode)
-            consoleMode &= ~(uint)(0b100_0000);
-
-            if (!SetConsoleMode(currentHandle, consoleMode))
-                throw new Exception("Failed to set console mode.");
+            DisableWindowResizing();
+            DisableTextSelection();
+            // DisableWindowTitleBar(); // TODO need to implement way of moving window when using this
         }
 
         public static Vector2 GetRelativeMousePosition()
@@ -67,6 +45,53 @@ namespace B.Utils
         {
             // private const int KEY_PRESSED = 0x8000;
             return Convert.ToBoolean(GetKeyState(VirtualKeyStates.VK_LBUTTON) & 0x8000);
+        }
+
+        #endregion
+
+
+
+        #region Private Methods
+
+        // https://social.msdn.microsoft.com/Forums/vstudio/en-US/1aa43c6c-71b9-42d4-aa00-60058a85f0eb/c-console-window-disable-resize?forum=csharpgeneral
+        private static void DisableWindowResizing()
+        {
+            _consoleHandle = GetConsoleWindow();
+
+            if (_consoleHandle != IntPtr.Zero)
+            {
+                IntPtr sysMenu = GetSystemMenu(_consoleHandle, false);
+                Enum.GetValues<SystemCommand>().ForEach(sc => DeleteMenu(sysMenu, (int)sc, 0x0000_0000));
+            }
+            else
+                throw new Exception("Failed to get console window handle.");
+        }
+
+        // https://stackoverflow.com/a/36720802
+        private static void DisableTextSelection()
+        {
+
+            // -10 is the standard input device
+            IntPtr currentHandle = GetStdHandle(-10);
+
+            if (!GetConsoleMode(currentHandle, out uint consoleMode))
+                throw new Exception("Failed to get console mode.");
+
+            // 0b1000000 (quick edit mode)
+            consoleMode &= ~(uint)(0b100_0000);
+
+            if (!SetConsoleMode(currentHandle, consoleMode))
+                throw new Exception("Failed to set console mode.");
+        }
+
+        // https://stackoverflow.com/a/2014514
+        private static void DisableWindowTitleBar()
+        {
+            int hWnd = FindWindow("ConsoleWindowClass", Program.Title);
+            long style = GetWindowLong(hWnd, -16L);
+            style &= ~(0b1100_0000_0000_0000_0000_0001L);
+            SetWindowLong(hWnd, -16L, style);
+            SetWindowPos(hWnd, 0L, 0L, 0L, 0L, 0L, 0x27L); // TODO try playing with 0x27L or -16L value to see if height can be adjusted
         }
 
         #endregion
@@ -133,6 +158,18 @@ namespace B.Utils
 
         [DllImport(USER32)]
         private static extern short GetKeyState(VirtualKeyStates nVirtKey);
+
+        [DllImport(USER32, EntryPoint = "GetWindowLongA")]
+        private static extern long GetWindowLong(long hWnd, long nIndex);
+
+        [DllImport(USER32, EntryPoint = "SetWindowLongA")]
+        private static extern long SetWindowLong(long hWnd, long nIndex, long dwNewLong);
+
+        [DllImport(USER32)]
+        private static extern int FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport(USER32)]
+        private static extern bool SetWindowPos(int hWnd, long hWndInsertAfter, long x, long y, long cx, long cy, long wFlags);
 
         #endregion
     }
